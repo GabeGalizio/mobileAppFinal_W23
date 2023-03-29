1e9604f908e49956d7cd01e555db31acb13d0c1c
daddymegaming <90657557+daddymegaming@users.noreply.github.com>
2023-03-28 22:31:56 -0400

    Custom Currency View Added

--- app/src/main/java/com/example/final_project/CharacterActivity.java
+++ app/src/main/java/com/example/final_project/CharacterActivity.java
@@ -10,12 +10,6 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.nio.channels.InterruptedByTimeoutException;
-import java.sql.Array;
-import java.util.ArrayList;
-
 public class CharacterActivity extends AppCompatActivity {
 
     Button btn;
@@ -50,26 +44,15 @@ public class CharacterActivity extends AppCompatActivity {
             --> Maybe make these custom if we want to make them look nice? I know you can do custom
                 views
          */
-        LinearLayout parent = findViewById(R.id.currencies_layout);
+        LinearLayout parent = findViewById(R.id.currencies_row);
         /* If we're editing currencies dynamically then this should be a for loop for each currency
             instead */
-        // ArrayList<EditText> currencies = new ArrayList<EditText>();
-        EditText cpET = new EditText(this);
-        EditText epET = new EditText(this);
-        EditText gpET = new EditText(this);
-        EditText ppET = new EditText(this);
-        EditText spET = new EditText(this);
-        //currencies.forEach((n) -> parent.addView(n));
-        parent.addView(cpET);
-        parent.addView(epET);
-        parent.addView(gpET);
-        parent.addView(ppET);
-        parent.addView(spET);
-        cpET.setText(Integer.toString(Cp));
-        epET.setText(Integer.toString(Ep));
-        gpET.setText(Integer.toString(Gp));
-        ppET.setText(Integer.toString(Pp));
-        spET.setText(Integer.toString(Sp));
+        CurrencyView cv = new CurrencyView(this);
+        parent.addView(cv);
+        cv.setLabelText("Test");
+        cv.setEditText("1");
+        CurrencyView cv2 = new CurrencyView(this);
+        parent.addView(cv2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,9 +68,6 @@ public class CharacterActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-    public void addCurrency(EditText et, int value) {
 
     }
 }
---
+++ app/src/main/java/com/example/final_project/CurrencyView.java
@@ -0,0 +1,56 @@
+package com.example.final_project;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+public class CurrencyView extends CardView {
+    private LinearLayout layout;
+    private EditText et;
+    private TextView tv;
+
+    public CurrencyView(@NonNull Context context) {
+        super(context);
+        layout = new LinearLayout(context);
+        et = new EditText(context);
+        tv = new TextView(context);
+        this.addView(layout);
+        // CARD PARAMS
+        this.setLayoutParams(new LayoutParams(300, 350));
+        this.setCardBackgroundColor(Color.parseColor("#4A9A9999"));
+        // LAYOUT PARAMS
+        layout.setOrientation(LinearLayout.VERTICAL);
+        ViewGroup.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        // EDIT TEXT PARAMS
+        et.setInputType(InputType.TYPE_NUMBER_VARIATION_NORMAL);
+        et.setText("0");
+        et.setTextSize(34);
+        et.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        // TEXTVIEW PARAMS
+        tv.setText("Currency Name");
+        tv.setTextSize(18);
+        tv.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        // Adding views to layout
+        layout.addView(et, params);
+        layout.addView(tv, params);
+    }
+
+    public void setEditText(String text) {
+        this.et.setText(text);
+    }
+
+    public void setLabelText(String text) {
+        this.tv.setText(text);
+    }
+}
